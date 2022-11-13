9706bb596235657235fa0e4dd116a39bb27b528a
treelanguage-Dev <treelanguage.dev@gmail.com>
2022-11-13 22:38:39 +0800

    fourth commit

--- main.cpp
+++ main.cpp
@@ -1,28 +1,10 @@
 #include <cstring>
-#include <cstdio>
-#include <string>
-#include <iostream>
 
-using namespace std;
 //
 // Created by treelanguage on 10/29/2022.
 //
 
-char compareStringLen(char str1[], char str2[]) {
-    int len1 = strlen(str1);
-    int len2 = strlen(str2);
-    if (len1 > len2) {
-        printf("1");
-    } else if (len1 < len2) {
-        printf("2");
-    } else {
-        printf("3");
-    }
-    return 0;
-}
-
-int main(){
-
-    compareStringLen("zhitingxuan","tingxuan");
+int main() {
+    char word[]={'H','e','i','i','o','!','\0'};
     return 0;
 }
